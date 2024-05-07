d03cd942dd601809332cf0622bea38487830141e
Eduardo Gonzalez <wm.eddie@gmail.com>
2024-05-07 16:58:12 -0700

    Update components.

--- multion_components.py
+++ multion_components.py
@@ -1,61 +1,81 @@
-from xai_components.base import InArg, OutArg, InCompArg, Component, BaseComponent, xai_component
-import multion
+from xai_components.base import InArg, OutArg, InCompArg, Component, BaseComponent, xai_component, secret
 import time
 import os
+import requests
 
+from multion.client import MultiOn
 
 
 @xai_component
 class MultiOnLogin(Component):
+    api_key: InArg[secret]
+    
     def execute(self, ctx):
-        ctx['_multion'] = multion.multion._multion_instance
-        if os.path.exists('multion_token.txt'):
-            # If it does, read the file
-            with open('multion_token.txt', 'r') as file:
-                token_content = file.read().strip()
-                # Set the token as a dictionary
-                ctx['_multion'].token = {'access_token': token_content}
-            time.sleep(5)
-        else:
-            multion.login()
-            print("Sleeping until multion_token.txt exists.")
-            while not os.path.exists('multion_token.txt'):
-                time.sleep(1)
+        multion = MultiOn(api_key=self.api_key.value)
+        ctx['_multion'] = multion
+        ctx['_multion_api_key'] = self.api_key.value
 
         
 @xai_component
 class MultiOnNewSession(Component):
     input_prompt: InCompArg[str]
     url: InCompArg[str]
+    local: InCompArg[bool]
+    max_steps: InArg[int]
     
-    tab_id: OutArg[int]
-    out_url: OutArg[str]
+    session_id: OutArg[str]
+    out_message: OutArg[str]
+    status: OutArg[str]
+    final_url: OutArg[str]
+    screenshot_url: OutArg[str]
+    step_count: OutArg[int]
+    processing_time: OutArg[int]
         
-    def execute(self, ctx):
-        response = multion.new_session({
-            "input": self.input_prompt.value,
-            "url": self.url.value
-        })
-        self.tab_id.value = int(response['tabId'])
-        self.out_url.value = response['url']
+    def execute(self, ctx) -> None:
+        multion = ctx['_multion']
+
+        browse = multion.browse(
+            cmd=self.input_prompt.value, 
+            url=self.url.value, 
+            local=self.local.value,
+            max_steps=self.max_steps.value if self.max_steps.value is not None else 20,
+            include_screenshot=True, 
+        )
+
+        self.session_id = browse.session_id
+        self.out_message.value = browse.message
+        self.status.value = browse.status
+        self.final_url.value = browse.url
+        self.screenshot_url.value = browse.screenshot
+        self.step_count = browse.metadata.step_count
+        self.processing_time.value = browse.metadata.processing_time
 
 
 @xai_component
-class MultiOnUpdateSession(Component):
+class MultiOnStepSession(Component):
     input_prompt: InCompArg[str]
-    url: InCompArg[str]
-    tab_id: InCompArg[int]
+    session_id: InCompArg[str]
+    max_steps: InArg[int]
         
-    out_url: OutArg[str]
-    screenshot: OutArg[str]
-    message: OutArg[str]
+    session_id: OutArg[str]
+    out_message: OutArg[str]
     status: OutArg[str]
+    final_url: OutArg[str]
+    screenshot_url: OutArg[str]
+    step_count: OutArg[int]
+    processing_time: OutArg[int]
         
-    def execute(self, ctx):        
-        response = multion.update_session(self.tab_id.value, {
-            "input": self.input_prompt.value, 
-            "url": self.url.value
-        })
+    def execute(self, ctx) -> None:
+        multion = ctx['_multion']
+        
+        browse = multion.browse(
+            cmd=self.input_prompt.value, 
+            url=self.url.value, 
+            local=self.local.value,
+            session_id=self.session_id.value,
+            max_steps=self.max_steps.value if self.max_steps.value is not None else 20,
+            include_screenshot=True, 
+        )
         
         self.out_url.value = response['url']
         self.screenshot.value = response['screenshot']
@@ -63,39 +83,32 @@ class MultiOnUpdateSession(Component):
         self.status.value = response['status']
 
 
-        
-@xai_component
-class MultiOnAutoRun(Component):
-    input_prompt: InCompArg[str]
-    url: InCompArg[str]
-        
-    out_url: OutArg[str]
-    screenshot: OutArg[str]
-    message: OutArg[str]
-    status: OutArg[str]
-        
-    def execute(self, ctx):  
-        response = multion.new_session({
-            "input": self.input_prompt.value,
-            "url": self.url.value
-        })
-            
-        tab_id = int(response['tabId'])
-        
-        status = response['status']
-        
-        tries = 0
-        while status == 'CONTINUE' and tries < 10:
-            response = multion.update_session(tab_id, {
-                "input": self.input_prompt.value + response['message'], 
-                "url": response['url']
-            })
-            
-            status = response['status']
-        
-            self.out_url.value = response['url']
-            self.screenshot.value = response['screenshot']
-            self.message.value = response['message']
-            self.status.value = response['status']
-            
-            tries += 1
+#Additional Components for Non-local APIs
+#
+#@xai_component
+#class MultiOnListSessions(Component):
+#    sessions: OutArg[list]
+#
+#    def execute(self, ctx) -> None:
+#        api_key = ctx['_multion_api_key']
+#
+#        url = f"https://api.multion.ai/v1/web/sessions"
+#        headers = {"X_MULTION_API_KEY": api_key}
+#
+#        response = requests.get(url, headers=headers)
+#        res = response.json()
+#        self.sessions.value = res['session_ids']
+#
+#
+#
+#@xai_component
+#class MultiOnDeleteSession(Component):
+#    session_id: InArg[str]
+#
+#    def execute(self, ctx) -> None:
+#        api_key = ctx['_multion_api_key']
+#
+#        url = f"https://api.multion.ai/v1/web/session/{self.session_id.value}"
+#        headers = {"X_MULTION_API_KEY": api_key}
+#
+#        response = requests.delete(url, headers=headers)
